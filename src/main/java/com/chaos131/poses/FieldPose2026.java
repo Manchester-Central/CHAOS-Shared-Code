759a871c5a959456260805b6ce51d9db7585f3a0
John <89701635+NinjaBuilderPro@users.noreply.github.com>
2026-02-07 11:55:45 -0500

    Hard coded field pose values

--- src/main/java/com/chaos131/poses/FieldPose2026.java
+++ src/main/java/com/chaos131/poses/FieldPose2026.java
@@ -2,18 +2,17 @@ package com.chaos131.poses;
 
 import static edu.wpi.first.units.Units.Inches;
 
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import java.io.IOException;
 
 public class FieldPose2026 extends PivotedFieldPose {
-  private static Translation2d Midpoint; // Must be initialized before any individual FieldPoses
+  private static Translation2d Midpoint =
+      new Translation2d(
+          16.540988 / 2, 8.069326 / 2); // Must be initialized before any individual FieldPoses
 
   public static final Translation3d BlueHubCenter =
       new Translation3d(Inches.of(181.56), Inches.of(158.32), Inches.of(72));
@@ -34,16 +33,24 @@ public class FieldPose2026 extends PivotedFieldPose {
   public static final Translation3d BlueClimbFarRight =
       new Translation3d(Inches.of(41.8), Inches.of(170.96), Inches.of(0));
 
-  private static final AprilTagFields AprilTagField = AprilTagFields.k2026RebuiltAndymark;
-  public static AprilTagFieldLayout AprilTagLayout;
-  public static FieldPose2026 Outpost;
-  public static FieldPose2026 HubCenter;
-  public static FieldPose2026 AllianceBallBox;
-  public static FieldPose2026 ClimbFarLeftBar;
-  public static FieldPose2026 ClimbLeftBar;
-  public static FieldPose2026 ClimbMiddle;
-  public static FieldPose2026 ClimbRightBar;
-  public static FieldPose2026 ClimbFarRightBar;
+  public static FieldPose2026 HubCenter =
+      new FieldPose2026(Alliance.Blue, "HubCenter", new Pose3d(BlueHubCenter, new Rotation3d()));
+  public static FieldPose2026 AllianceBallBox =
+      new FieldPose2026(
+          Alliance.Blue, "AllianceBallBox", new Pose3d(BlueAllianceBallBox, new Rotation3d()));
+  public static FieldPose2026 ClimbFarLeftBar =
+      new FieldPose2026(
+          Alliance.Blue, "FarLeftClimb", new Pose3d(BlueClimbFarLeft, new Rotation3d()));
+  public static FieldPose2026 ClimbLeftBar =
+      new FieldPose2026(Alliance.Blue, "LeftClimb", new Pose3d(BlueClimbLeft, new Rotation3d()));
+  public static FieldPose2026 ClimbMiddle =
+      new FieldPose2026(
+          Alliance.Blue, "MiddleClimb", new Pose3d(BlueClimbMiddle, new Rotation3d()));
+  public static FieldPose2026 ClimbRightBar =
+      new FieldPose2026(Alliance.Blue, "RightClimb", new Pose3d(BlueClimbRight, new Rotation3d()));
+  public static FieldPose2026 ClimbFarRightBar =
+      new FieldPose2026(
+          Alliance.Blue, "FarRightClimb", new Pose3d(BlueClimbFarRight, new Rotation3d()));
 
   public FieldPose2026(Alliance defaultAlliance, String name, Pose3d defaultPose) {
     super(Midpoint, defaultAlliance, name, defaultPose);
@@ -53,41 +60,5 @@ public class FieldPose2026 extends PivotedFieldPose {
     super(Midpoint, defaultAlliance, name, new Pose3d(defaultPose));
   }
 
-  /**
-   * Required function before using any field points. Necessary because Java's static function
-   * initializers will fail to load any of it. Values will be null. Run this at runtime to load the
-   * data files early in the process.
-   *
-   * <p>Unfortunately, this means we can't make these fields final. You can thank Java for that.
-   *
-   * @throws IOException
-   */
-  public static void initializeFieldPoses() throws IOException {
-    AprilTagLayout = new AprilTagFieldLayout(AprilTagField.m_resourceFile);
-    Midpoint =
-        new Translation2d(
-            AprilTagLayout.getFieldLength() / 2.0, AprilTagLayout.getFieldWidth() / 2.0);
-    // Individual Field Poses
-    HubCenter =
-        new FieldPose2026(Alliance.Blue, "HubCenter", new Pose3d(BlueHubCenter, new Rotation3d()));
-    Outpost = new FieldPose2026(Alliance.Blue, "Outpost", AprilTagLayout.getTagPose(29).get());
-    AllianceBallBox =
-        new FieldPose2026(
-            Alliance.Blue, "AllianceBallBox", new Pose3d(BlueAllianceBallBox, new Rotation3d()));
-    // Climber positions
-    ClimbFarLeftBar =
-        new FieldPose2026(
-            Alliance.Blue, "FarLeftClimb", new Pose3d(BlueClimbFarLeft, new Rotation3d()));
-    ClimbLeftBar =
-        new FieldPose2026(Alliance.Blue, "LeftClimb", new Pose3d(BlueClimbLeft, new Rotation3d()));
-    ClimbMiddle =
-        new FieldPose2026(
-            Alliance.Blue, "MiddleClimb", new Pose3d(BlueClimbMiddle, new Rotation3d()));
-    ClimbRightBar =
-        new FieldPose2026(
-            Alliance.Blue, "RightClimb", new Pose3d(BlueClimbRight, new Rotation3d()));
-    ClimbFarLeftBar =
-        new FieldPose2026(
-            Alliance.Blue, "FarRightClimb", new Pose3d(BlueClimbFarRight, new Rotation3d()));
-  }
+  // Outpost = new FieldPose2026(Alliance.Blue, "Outpost", AprilTagLayout.getTagPose(29).get());
 }
