ddbb717fe867967e9e794e8afe0e53d2214b7aec
Joshua Allard <jnallard@users.noreply.github.com>
2026-02-07 11:52:09 -0500

    Adds Color8Bit and fromDutyCycle to Color enum

--- src/main/java/com/chaos131/util/Color.java
+++ src/main/java/com/chaos131/util/Color.java
@@ -1,5 +1,7 @@
 package com.chaos131.util;
 
+import edu.wpi.first.wpilibj.util.Color8Bit;
+
 /** Colors stored in RGB format */
 public enum Color {
   /** Red */
@@ -19,6 +21,8 @@ public enum Color {
   /** Purple */
   PURPLE(255, 0, 255);
 
+  public Color8Bit color8Bit;
+
   /** Color's red channel */
   public int red;
 
@@ -28,10 +32,35 @@ public enum Color {
   /** Color's blue channel */
   public int blue;
 
-  /** Forms a color out of numbers */
+  /** Forms a Color out of numbers */
   Color(int r, int g, int b) {
-    red = r;
-    green = g;
-    blue = b;
+    this(new Color8Bit(r, g, b));
+  }
+
+    /** Forms a Color out of a Color8BIt */
+  Color(Color8Bit c) {
+    color8Bit = c;
+    red = c.red;
+    green = c.green;
+    blue = c.blue;
+  }
+
+  /**
+   * Returns a COlor8Bit that shows scaled values for the duty cycle
+   * @param dutyCycle a percentage value [-1.0, 1.0] to convert to a scaled color
+   */
+  public static Color8Bit fromDutyCycle(double dutyCycle) {
+    int grayBase = 131;
+    // If duty cycle is zero, show gray
+    if (dutyCycle == 0) {
+      return new Color8Bit(grayBase, grayBase, grayBase);
+    }
+
+    int scaleValue = (int) Math.floor(100 * dutyCycle);
+    int scaledUpValue = grayBase + scaleValue + 10;
+    int scaledDownValue = grayBase - scaleValue - 10;
+    return dutyCycle > 0 
+      ? new Color8Bit(scaledDownValue, scaledUpValue, scaledDownValue) // Show a scaled green value if positive
+      : new Color8Bit(scaledUpValue, scaledDownValue, scaledDownValue); // Show a scaled red value if negatve
   }
 }
