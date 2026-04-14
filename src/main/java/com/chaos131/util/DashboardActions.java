60d01bf83b511620a2f12d111f89427d2c6b9162
Pathal <Pathal@users.noreply.github.com>
2026-04-13 23:12:28 -0400

    Syntactic Sugar for organizing runnables in a dashboard chooser

---
+++ src/main/java/com/chaos131/util/DashboardActions.java
@@ -0,0 +1,22 @@
+package com.chaos131.util;
+
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
+
+/**
+ * Simple syntactic sugar to automatically trigger runnables when a chooser changes state. Ex.
+ * <pre>{@code var chooser = new DashboardActions("TopicGroup/Topic",
+ *  "Enable", () -> robotFlag = true);
+ *chooser.addOption("Disable", () -> robotFlag = false);
+ *return chooser;}</pre>
+ */
+public class DashboardActions extends LoggedDashboardChooser<Runnable> {
+  public DashboardActions(String name, String default_action_name, Runnable default_task) {
+    super(name);
+    addOption(default_action_name, default_task);
+    onChange(this::doAction);
+  }
+
+  private void doAction(Runnable task) {
+    task.run();
+  }
+}
